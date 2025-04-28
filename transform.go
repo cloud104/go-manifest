27ba01c7c64a32efbe6baaf27608ba2f1270fd06
Tiago Angelo <tiago.angelo@totvs.com.br>
2025-04-28 17:28:33 -0300

    Twenty-third commit

--- transform.go
+++ transform.go
@@ -1,6 +1,12 @@
 package manifest
 
-import "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+import (
+	"fmt"
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+)
 
 type Transformer func(u *unstructured.Unstructured) error
 
@@ -19,4 +25,45 @@ func (l *list) Transform(funcs ...Transformer) (List, error) {
 	}
 
 	return &list{resources: resources, fieldManager: l.fieldManager, client: l.client, mapper: l.mapper}, nil
+}
+
+// Object is a generic transformer that applies a user-defined function 'fn'
+// to a Kubernetes runtime.Object, handling conversion from/to unstructured form.
+func Object[T runtime.Object](fn func(obj T) error) Transformer {
+	return func(u *unstructured.Unstructured) error {
+		// Create a new instance of the type
+		var t T
+		tType := reflect.TypeOf(t)
+
+		if tType == nil {
+			return fmt.Errorf("cannot create instance of nil type")
+		}
+
+		var obj T
+		if tType.Kind() == reflect.Ptr {
+			obj = reflect.New(tType.Elem()).Interface().(T)
+		} else {
+			obj = t
+		}
+
+		// Convert the unstructured object into the typed object
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
+			return fmt.Errorf("failed to convert %T to %T: %w", u, obj, err)
+		}
+
+		// Apply the user-provided function
+		if err := fn(obj); err != nil {
+			return err
+		}
+
+		// Convert the object back into an unstructured map
+		unstr, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+		if err != nil {
+			return fmt.Errorf("failed to convert %T to %T: %w", obj, u, err)
+		}
+
+		u.SetUnstructuredContent(unstr)
+
+		return nil
+	}
 }
